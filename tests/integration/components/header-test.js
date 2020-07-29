991f89afb9ba45595960253c295855f93aac43cb
Damien White <damien.white@visoftinc.com>
2020-07-29 08:56:14 -0400

    Corrected failing component test

--- tests/integration/components/header-test.js
+++ tests/integration/components/header-test.js
@@ -7,20 +7,8 @@ module('Integration | Component | header', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    await render(hbs`<Header @title="Foo" />`);
 
-    await render(hbs`<Header />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <Header>
-        template block text
-      </Header>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.textContent.trim(), "Foo\n\nHome\nBeers");
   });
 });
